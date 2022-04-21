ebe6f74e46635f5457b38dfafe5e3d2a15478fa6
oleg <cityavto0137@gmail.com>
2022-04-21 09:34:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+require('dotenv').config();
+require('./models/db');
+const userRouter = require('./routes/user');
+
+const User = require('./models/user');
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+// app.use((req, res, next) => {
+//   req.on('data', chunk => {
+//     const data = JSON.parse(chunk);
+//     req.body = data;
+//     next();
+//   });
+// });
+
+app.use(express.json());
+app.use(userRouter);
+
+// const test = async (email, password) => {
+//   const user = await User.findOne({ email: email });
+//   const result = await user.comparePassword(password);
+//   console.log(result);
+// };
+
+// test('niraj@email.com', 'niraj12');
+
+app.get('/test', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'Welcome to backend zone!' });
+});
+
+app.listen(8000, () => {
+  console.log('port is listening');
+});
---
+++ helper/imageUpload.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name:'dzubnan0v',
+  api_key:'196857716592863',
+  api_secret:'FyxCaxelawGuOYrzift0UuTYrfk',
+});
+
+module.exports = cloudinary;
